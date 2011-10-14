8db750d7b99829198610a335184e0bfc8b88236f
Michael Armbruster <Blitzi94@gmx.de>
2011-10-14 20:30:57 +0200

    Created the Singleplayer Client Project for MonoDevelop

---
+++ src/SP_Client/Main.cs
@@ -0,0 +1,16 @@
+using System;
+using Gtk;
+
+namespace SP_Client
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Application.Init ();
+			MainWindow win = new MainWindow ();
+			win.Show ();
+			Application.Run ();
+		}
+	}
+}
